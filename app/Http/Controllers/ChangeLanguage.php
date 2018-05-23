8c3491add9e14000fabaa48ab18365accce6f3c8
Salma Omar <salmaomar@pentavalue.com>
2018-05-23 14:59:46 +0200

    change lang

--- app/Http/Controllers/ChangeLanguage.php
+++ app/Http/Controllers/ChangeLanguage.php
@@ -12,7 +12,9 @@ class ChangeLanguage extends Controller
     public function changeLang(Request $request) {
 
         $url = $request->url;
+        
         $segment = $request->segment;
+        dd($segment);
         $newSegment = $segment == 'ar' ? 'en' : 'ar';
 
         $newURL  = $segment == 'ar' ? preg_replace("/ar\b/", 'en', $url) : preg_replace("/en\b/", 'ar', $url);
