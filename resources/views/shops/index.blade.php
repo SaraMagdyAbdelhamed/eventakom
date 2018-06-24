30f004e722460ca0d1cffb6eae306e6478c24bdd
Salma Omar <salmaomar@pentavalue.com>
2018-06-24 23:53:30 +0200

    index

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -2,21 +2,107 @@
 @section('content')
 <!-- =============== Custom Content ===========-==========-->
 <div class="row">
-  <div class="col-xs-12">
-    <div class="cover-inside-container margin--small-top-bottom bradius--no bshadow--0" style="background-image:  url( {{ asset('img/covers/dummy2.jpg ') }} )  ; background-position: center center; background-repeat: no-repeat; background-size:cover;">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="text-xs-center">         
-            <div class="text-wraper">
-              <h3 class="cover-inside-title  ">Shop and dine</h3>
+    <div class="col-xs-12">
+      <div class="cover-inside-container margin--small-top-bottom bradius--no bshadow--0" style="background-image:  url( {{ asset('img/covers/dummy2.jpg ') }} )  ; background-position: center center; background-repeat: no-repeat; background-size:cover;">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="text-xs-center">         
+              <div class="text-wraper">
+                <h3 class="cover-inside-title  ">Shop and dine</h3>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="cover--actions"><a class="bradius--no border-btn master-btn" type="button" href="{{route('add_shop')}}">Add new Shop and dine</a>
+          <div class="cover--actions"><a class="bradius--no border-btn master-btn" type="button" href="{{route('add_shop')}}">Add new Shop and dine</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+    <div class="col-xs-12">
+      <div class="cardwrap inherit bradius--noborder bshadow--0 padding--small margin--small-top-bottom">
+        <div class="full-table">
+          <div class="filter__btns"><a class="filter-btn master-btn" href="#filter-users"><i class="fa fa-filter"></i>filters</a></div>
+          <div class="bottomActions__btns"><a class="master-btn" href="#">Delete selected</a><a class="master-btn" href="#">Add new Shop and dine</a>
+          </div>
+          <div class="remodal" data-remodal-id="filter-users" role="dialog" aria-labelledby="modal1Title" aria-describedby="modal1Desc">
+            <button class="remodal-close" data-remodal-action="close" aria-label="Close"></button>
+            <div>
+              <div class="row">
+                <div class="col-sm-6 col-xs-12">
+                  <div class="master_field">
+                    <label class="master_label" for="filter_cat">place categories </label>
+                    <select class="master_input select2" id="filter_cat" multiple="multiple" data-placeholder="place categories" style="width:100%;" ,>
+                      <option>historical</option>
+                      <option>meditations</option>
+                      <option>Etc</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                  <div class="master_field">
+                    <label class="master_label">place status</label>
+                    <div class="funkyradio">
+                      <input type="checkbox" id="event_status_2">
+                      <label for="event_status_2">Active</label>
+                    </div>
+                    <div class="funkyradio">
+                      <input type="checkbox" id="event_status_3">
+                      <label for="event_status_3">inActive</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div><br>
+            <button class="remodal-cancel" data-remodal-action="cancel">Cancel</button>
+            <button class="remodal-confirm" data-remodal-action="confirm">Apply Filters</button>
+          </div>
+          <form id="dataTableTriggerId_001_form">
+            <table class="data-table-trigger table-master" id="dataTableTriggerId_001">
+              <thead>
+                <tr class="bgcolor--gray_mm color--gray_d">
+                  <th><span class="cellcontent">&lt;input type=&quot;checkbox&quot; data-click-state=&quot;0&quot; name=&quot;select-all&quot; id=&quot;select-all&quot; /&gt;</span></th>
+                  <th><span class="cellcontent">serial No</span></th>
+                  <th><span class="cellcontent">Shop photo</span></th>
+                  <th><span class="cellcontent">Place name</span></th>
+                  <th><span class="cellcontent">Place phone</span></th>
+                  <th><span class="cellcontent">status</span></th>
+                  <th><span class="cellcontent">actions</span></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><span class="cellcontent"></span></td>
+                  <td><span class="cellcontent">5</span></td>
+                  <td><span class="cellcontent"><img src = "https://source.unsplash.com/random" , class = " img-in-table"></span></td>
+                  <td><span class="cellcontent">El batraa jordan</span></td>
+                  <td><span class="cellcontent">0123456789</span></td>
+                  <td><span class="cellcontent"><i class = "fa icon-in-table-true fa-check"></i><i class = "fa icon-in-table-false fa-times"></i></span></td>
+                  <td><span class="cellcontent"><a href= #popupModal_1 ,  class= "action-btn bgcolor--main color--white "><i class = "fa  fa-eye"></i></a><a href= shop_and_dine_edit.html ,  class= "action-btn bgcolor--fadegreen color--white "><i class = "fa  fa-pencil"></i></a><a href="#"  class= "btn-warning-confirm action-btn bgcolor--fadebrown color--white "><i class = "fa  fa-trash-o"></i></a></span></td>
+                </tr>
+                <tr>
+                  <td><span class="cellcontent"></span></td>
+                  <td><span class="cellcontent">5</span></td>
+                  <td><span class="cellcontent"><img src = "https://source.unsplash.com/random" , class = " img-in-table"></span></td>
+                  <td><span class="cellcontent">El batraa jordan</span></td>
+                  <td><span class="cellcontent">0123456789</span></td>
+                  <td><span class="cellcontent"><i class = "fa icon-in-table-true fa-check"></i><i class = "fa icon-in-table-false fa-times"></i></span></td>
+                  <td><span class="cellcontent"><a href= #popupModal_1 ,  class= "action-btn bgcolor--main color--white "><i class = "fa  fa-eye"></i></a><a href= shop_and_dine_edit.html ,  class= "action-btn bgcolor--fadegreen color--white "><i class = "fa  fa-pencil"></i></a><a href="#"  class= "btn-warning-confirm action-btn bgcolor--fadebrown color--white "><i class = "fa  fa-trash-o"></i></a></span></td>
+                </tr>
+                <tr>
+                  <td><span class="cellcontent"></span></td>
+                  <td><span class="cellcontent">5</span></td>
+                  <td><span class="cellcontent"><img src = "https://source.unsplash.com/random" , class = " img-in-table"></span></td>
+                  <td><span class="cellcontent">El batraa jordan</span></td>
+                  <td><span class="cellcontent">0123456789</span></td>
+                  <td><span class="cellcontent"><i class = "fa icon-in-table-true fa-check"></i><i class = "fa icon-in-table-false fa-times"></i></span></td>
+                  <td><span class="cellcontent"><a href= #popupModal_1 ,  class= "action-btn bgcolor--main color--white "><i class = "fa  fa-eye"></i></a><a href= shop_and_dine_edit.html ,  class= "action-btn bgcolor--fadegreen color--white "><i class = "fa  fa-pencil"></i></a><a href="#"  class= "btn-warning-confirm action-btn bgcolor--fadebrown color--white "><i class = "fa  fa-trash-o"></i></a></span></td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+          <button id="delete-test">Delete Tests</button>
+        </div>
+      </div>
+    </div><br>    
 </div>
            
 
